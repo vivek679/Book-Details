3d46fa9ffa908b1560818016c83be001e31ed69e
VIVEK KUMAR <32561934+vivek679@users.noreply.github.com>
2019-05-19 13:07:36 +0530

    Book.java

---
+++ Book.java
@@ -0,0 +1,28 @@
+public class Book{
+  private String bookName;
+  private int bookPrice;
+  private String authorName;
+  
+  public String getBookName(){
+      return bookName;
+  }
+  public int getBookPrice(){
+      return bookPrice;
+  }
+  public String getAuthorName(){
+      return authorName;
+  }
+
+  public void setBookName(String bookName){
+      this.bookName=bookName;
+  }
+  public void setBookPrice(int bookPrice){
+      this.bookPrice=bookPrice;
+  }
+  public void setAuthorName(String authorName){
+      this.authorName=authorName;
+  }
+
+
+
+
